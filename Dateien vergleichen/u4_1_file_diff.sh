8f9aa1cb6003469f2a8bbf79ba484e08433cebc2
Arnel Deomic <deomicarnel@hotmail.com>
2021-10-04 14:14:37 +0200

    u4_1_file_diff.sh added check if file1 and file2 exist

--- Dateien vergleichen/u4_1_file_diff.sh
+++ Dateien vergleichen/u4_1_file_diff.sh
@@ -18,49 +18,54 @@ echo "Dokument 1: $1"
 echo "Dokument 2: $2"
 echo ""
 echo "************************************"
-echo "Ausgabe der Zeilen:"
 
-# Variablen setzen
-# Überprüfung welches Dokument mehr Zeilen hat (-gt ist greater than)
-if [[ $(wc -l < "$1") -gt $(wc -l < "$2") ]];
-then
-	biggerFile=$1
-	smallerFile=$2
+#Überprüfung ob beide Dokumente existieren
+if [[ -f "$1" && -f "$2" ]]; then
+  echo "Ausgabe der Zeilen:"
+  # Variablen setzen
+  # Überprüfung welches Dokument mehr Zeilen hat (-gt ist greater than)
+  if [[ $(wc -l < "$1") -gt $(wc -l < "$2") ]];
+  then
+    biggerFile=$1
+    smallerFile=$2
+  else
+    biggerFile=$2
+    smallerFile=$1
+  fi
+
+  #Aus grossen Dokument Anzahl Zeilen holen. +1 weil die Länge bei 0 beginnt.
+  differentRows=0
+  amountRowsBiggerFile=$(("$(wc -l < "$biggerFile")"+1))
+
+  #Zeile bei dem der While starten soll
+  i=1
+  #While dass durch die Zeilen von eins bis amountRowsBiggerFile durchgeht.
+  while [[ $i -le $amountRowsBiggerFile ]]
+  do
+    #Aktuelle Zeile des grossen Dokumentes
+    currentRowBiggerFile=$(sed -n ${i}p "$biggerFile")
+    #Aktuelle Zeile des kleinen Dokumentes
+    currentRowSmallerFile=$(sed -n ${i}p "$smallerFile")
+
+    echo "------------------------------------"
+    echo "$i: $currentRowBiggerFile"
+    echo "$i: $currentRowSmallerFile"
+    echo "------------------------------------"
+
+    #Überprüfung ob aktuelle Zeile von beiden Dokumenten nicht gleich ist
+    if [[ "$currentRowBiggerFile" != "$currentRowSmallerFile" ]]; then
+      ((differentRows=differentRows+1))
+
+      echo "Zeile $i nicht gleich"
+    fi
+    ((i=i+1))
+  done
+
+  echo ""
+  echo "************************************"
+  echo "Insgesamt sind $differentRows Zeilen ungleich"
+  echo ""
+  echo "************************************"
 else
-	biggerFile=$2
-	smallerFile=$1
+  echo "Ungültige Dokumentangabe"
 fi
-
-#Aus grossen Dokument Anzahl Zeilen holen. +1 weil die Länge bei 0 beginnt.
-differentRows=0
-amountRowsBiggerFile=$(("$(wc -l < "$biggerFile")"+1))
-
-#Zeile bei dem der While starten soll
-i=1
-#While dass durch die Zeilen von eins bis amountRowsBiggerFile durchgeht.
-while [[ $i -le $amountRowsBiggerFile ]]
-do
-  #Aktuelle Zeile des grossen Dokumentes
-  currentRowBiggerFile=$(sed -n ${i}p "$biggerFile")
-  #Aktuelle Zeile des kleinen Dokumentes
-	currentRowSmallerFile=$(sed -n ${i}p "$smallerFile")
-
-  echo "------------------------------------"
-	echo "$i: $currentRowBiggerFile"
-	echo "$i: $currentRowSmallerFile"
-  echo "------------------------------------"
-
-  #Überprüfung ob aktuelle Zeile von beiden Dokumenten nicht gleich ist
-	if [[ "$currentRowBiggerFile" != "$currentRowSmallerFile" ]]; then
-	  ((differentRows=differentRows+1))
-
-		echo "Zeile $i nicht gleich"
-	fi
-  ((i=i+1))
-done
-
-echo ""
-echo "************************************"
-echo "Insgesamt sind $differentRows Zeilen ungleich"
-echo ""
-echo "************************************"
